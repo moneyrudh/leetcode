9ae7fc02f124a2f4df8d18b9290e6defc090e583
Anirudh Ananthasubramaniam <anirudhananth99@gmail.com>
2025-04-11 01:07:55 -0400

    27

---
+++ 27.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i, j, k = 0, 0, 0
+        n = len(nums)
+
+        while i < n:
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+                k += 1
+            i += 1
+
+        return k
